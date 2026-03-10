1d26e3c77158b4c2e9673f1d96cdce97e2dbdb44
DevaPrakash06 <Devaprakash0822@gmail.com>
2026-03-10 15:26:19 +0530

    Base Application Setup

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Book My Stay App
+ * Hotel Booking Management System
+ *
+ * This class represents the entry point of the application.
+ * It prints a welcome message along with the application name
+ * and version when the program starts.
+ *
+ * @author S.M. Deva Prakash
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking Management System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=================================");
+
+        System.out.println("Application started successfully.");
+    }
+}
